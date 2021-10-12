4cd2094291d61da016bc3caf9e5d7647febf2261
Jaime-Fernandez <jaime.uxd@gmail.com>
2021-10-11 21:01:11 -0500

    Updated img, links and form

--- script/main.js
+++ script/main.js
@@ -2,3 +2,18 @@ function showMenu() {
   document.getElementById('hamburger').classList.toggle('isActive')
   document.getElementById('navMenu').classList.toggle('isVisible');
 }
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
