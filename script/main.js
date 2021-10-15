0df3add0b3dd7dc63d2ed1b2e04d802c48985873
Jaime-Fernandez <jaime.uxd@gmail.com>
2021-10-14 20:05:49 -0500

    Added modal for contact form

--- script/main.js
+++ script/main.js
@@ -3,17 +3,6 @@ function showMenu() {
   document.getElementById('navMenu').classList.toggle('isVisible');
 }
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
+function closeModal(){
+  document.getElementById('modal').classList.remove('showModal');
 }
